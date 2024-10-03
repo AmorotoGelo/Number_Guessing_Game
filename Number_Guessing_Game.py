274789679b1426122a2d8300bfa05c7092914e0f
Carl Angelo Amoroto <carlamoroto200526@gmail.com>
2024-10-03 08:37:29 +0800

    If you type zero or lower than zero, the game won't proceed

--- Number_Guessing_Game.py
+++ Number_Guessing_Game.py
@@ -1,3 +1,13 @@
 import random
 
 top_of_range = input("Type a number: ")
+
+if top_of_range.isdigit():
+     top_of_range = int(top_of_range)
+
+     if top_of_range <= 0:
+          print('Please type a number larger than zero next time. ')
+          quit()
+else:
+     print('Please type a number next time. ')
+     quit()          
